5e85a8360a226403f62014e9d8fd29be6a420a77
Sagar Pande <shaggy.pande@gmail.com>
2019-09-14 20:17:05 +0530

    sps

--- sps.sh
+++ sps.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 opt=(Stone Paper Scissor)
-
+valid="[123]"
 
 ##Welcome message
 echo "****Welcome to the Stone Paper Scissor Game*******"
-sleep 2
+echo "In case you are new"
+echo " *************"
+echo "Stone Beats Paper"
+echo "Paper beats scissor"
+echo "Scissor beat stone"
 
 
 ##Ask for User choice
@@ -19,11 +23,33 @@ done
 #echo "2. Paper"
 #echo "3. Scissor"
 
+
+## Read User Input
 read usr_opt
+
+
+
+##  Vaidate User Input
+if [ -z $usr_opt ] ;then
+echo "Plese enter Option Amongst 1,2 and 3"
+exit
+fi
+
+
+## Check for Valid input
+if [[ ! $valid =~ $usr_opt ]]; then
+echo "Please enter valid input"
+
+exit
+
+fi
 
 ##Randomly choose Option in shell
 comp_opt=${opt[RANDOM%3]}
-echo $comp_opt
+
+
+## Print Computer's randomly selected choice
+echo Computer Chose $comp_opt
 
 ##Compare and decide Options
 case $comp_opt in 
@@ -46,9 +72,9 @@ case $comp_opt in
         ;;
 
         Scissor)
-        if [ $usr_opt == "3" ] ;then
+        if [ $usr_opt -eq "3" ] ;then
         echo "Same Options, no Winner Here, try again"
-        elif [ $usr_opt == "2" ];then
+        elif [ $usr_opt -eq "2" ];then
         echo "Computer Won, You loser $USER"
         else echo "You won $USER, Congratulations"
         fi
